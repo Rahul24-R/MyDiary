bed6200a2ca2e9641b008d4c810b8de32461ed07
Rahul <beingunique2000@gmail.com>
2024-06-16 07:38:12 +0530

    forgot password component

--- myDiary/src/app/Pages/forgot-password/forgot-password.component.ts
+++ myDiary/src/app/Pages/forgot-password/forgot-password.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { passwordmodel } from './password.model';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-forgot-password',
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class ForgotPasswordComponent {
 
+  resetmodel:passwordmodel = new passwordmodel("","")
+
+  constructor(private spinner:NgxSpinnerService) { }
+  resetpassword() {
+    this.spinner.show();
+    }
 }
---
+++ myDiary/src/app/Pages/forgot-password/password.model.ts
@@ -0,0 +1,6 @@
+export class passwordmodel{
+    constructor(
+        public username:string,
+        public email:string
+    ){}
+}
